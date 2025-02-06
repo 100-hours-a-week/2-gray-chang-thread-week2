2a6da8ddee4cf5e9625d804f76a6f806b993f685
Yehun <yhyh4420@naver.com>
2025-02-06 22:52:24 +0900

    test: 공학용 계산기 테스트 로직 추가

--- src/test/java/controller/ControllerTest.java
+++ src/test/java/controller/ControllerTest.java
@@ -4,6 +4,7 @@ import calculatorEnum.CalculatorEnum;
 import calculatorEnum.MethodName;
 import calculatorService.Calculator;
 import calculatorService.NormalCalculator;
+import calculatorService.ScientificCalculator;
 import orderObject.Order;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -14,8 +15,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class ControllerTest {
 
     @Test
-    @DisplayName("정상적인 연산을 하는지 테스트")
-    void makeAnswer() {
+    @DisplayName("일반계산기, 파라미터를 받을 때 정상적인 연산을 하는지 테스트")
+    void makeAnswerNormalCalculator() {
         Controller controller = new Controller();
         //given
         CalculatorEnum calculatorEnum = CalculatorEnum.Normal_calculator;
@@ -29,5 +30,27 @@ class ControllerTest {
 
         //then
         Assertions.assertThat(answer).isEqualTo(Double.valueOf(firstNum + secondNum));
+    }
+
+    @Test
+    @DisplayName("공학용계산기, 파라미터 객체를 받을 때 정상적인 연산을 하는지 테스트")
+    void makeAnswerScientificCalculator() {
+        Controller controller = new Controller();
+        //given
+        CalculatorEnum calculatorEnum = CalculatorEnum.Scientific_calculator;
+        int firstNum = 1;
+        int secondNum = 2;
+
+        //when
+        Order orderPlus = new Order(calculatorEnum, MethodName.plus, firstNum, secondNum);
+        Order orderSin = new Order(calculatorEnum, MethodName.sin, firstNum);
+        Calculator calculator = new ScientificCalculator(firstNum, secondNum);
+        Calculator calculatorTrival = new ScientificCalculator(firstNum);
+        double answerPlus = controller.makeAnswer(orderPlus, calculator);
+        double answerSin = controller.makeAnswer(orderSin, calculatorTrival);
+
+        //then
+        Assertions.assertThat(answerPlus).isEqualTo(Double.valueOf(firstNum + secondNum));
+        Assertions.assertThat(answerSin).isEqualTo(Math.sin(Math.toRadians(firstNum)));
     }
 }
