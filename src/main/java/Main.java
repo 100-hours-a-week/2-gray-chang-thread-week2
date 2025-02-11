4fc7f526768a55a556b1ca4e2d107cd1b91c7386
Yehun <yhyh4420@naver.com>
2025-02-11 11:56:38 +0900

    feat: 저작권 없는 테스트음악 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import music.MusicPlayer;
 
 public class Main {
     public static void main(String[] args) {
-        String filePath = "src/main/java/music/yudabinband_hanghhae.WAV";
+        String filePath = "src/main/java/music/test_music.WAV";
         MusicPlayer musicPlayer = new MusicPlayer(filePath);
         Thread thread = new Thread(musicPlayer);
         thread.setDaemon(true);
