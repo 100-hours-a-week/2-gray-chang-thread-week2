e81b3861cdbfa0e30ce95f9d91339de2df20d993
Yehun <yhyh4420@naver.com>
2025-02-12 00:01:20 +0900

    fix: 음악 스레드 종료 안되는 로직 해결

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ public class Main {
         String filePath = "src/main/java/music/Mediterranean-View-Everet-Almond.wav";
         MusicPlayer musicPlayer = new MusicPlayer(filePath);
         Thread musicThread = new Thread(musicPlayer);
+        musicThread.setDaemon(true);
         musicThread.start();
 
         ControllerThread controller = new ControllerThread();
